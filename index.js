d43b652234bb20ae6f09e30defa880ac64216e76
Md Kaif Sardar <147906935+MdKaifSardar@users.noreply.github.com>
2024-07-03 02:59:33 +0530

    removed dist

--- index.js
+++ index.js
@@ -2,27 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
-// var cors = require('cors');
 const dbPass = process.env.MONGODB_PASS;
 const ResumeRoute = require('./routes/resume-query');
 const JobRoute = require('./routes/job-finder');
-
-
-// app.use(cors({
-//   origin: 'https://skill-sync-jet.vercel.app',
-//   methods: ['POST', 'GET'],
-//   credentials: true
-// }));
-// app.options('*', cors());
-app.use(express.static('dist'));
 app.use(express.json());
 
 app.use('/api/resume', ResumeRoute);
 app.use('/api/job', JobRoute);
 
-// app.get('/', (req, res) => {
-//     res.send("hello the user");
-// });
+app.get('/', (req, res) => {
+    res.send("hello the user");
+});
 
 
 app.listen(PORT, () => {
